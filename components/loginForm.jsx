31d752f4bc6f251fb4dc88bd50aa537894569e66
wout <galaxic.dev@gmail.com>
2025-02-26 10:25:02 +0100

    commit

--- components/loginForm.jsx
+++ components/loginForm.jsx
@@ -43,13 +43,13 @@ const LoginForm = () => {
         setError("");
         Cookies.set('token', data.token, {
           expires: 31,
-          secure: true,
+          secure: false,
           sameSite: 'Strict'
         });
         console.log("set cookie token to:", data.token);
         Cookies.set('user', JSON.stringify({ _id: data.user._id, name: data.user.name, email: data.user.email }), {
           expires: 31,
-        secure: true,
+        secure: false,
         sameSite: 'Strict'
         });
       }
