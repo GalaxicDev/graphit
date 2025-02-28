d0b7ff32641172cc9243c18645e74415cf77050d
wout <galaxic.dev@gmail.com>
2025-02-28 14:15:50 +0100

    added userlist

--- app/(root)/admin/page.js
+++ app/(root)/admin/page.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import { AdminUserList } from "@/components/AdminUserList"
-import { fetchUsers } from "@/lib/user"
+import { fetchUsers, verifyToken } from "@/lib/api"
 import { cookies } from 'next/headers'
 
 const page = async () => {
 
   const cookieStore = await cookies();
   const token = cookieStore.get('token')?.value;
-  const allUsers = await fetchUsers(token);
+
+  // check if the user is admin
+  const userData = await verifyToken(token);
+  const userRole = userData.user.role;
+  let allUsers = [];
+
+  if (userRole === "admin") {
+    allUsers = await fetchUsers(token);
+  }
+
 
   return (
     <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-bold mb-6 dark:text-white">Admin Panel</h1>
-      <AdminUserList token={token} users={allUsers} />
+      {userRole === "admin" ? (
+        <>
+          <h1 className="text-3xl font-bold mb-6 dark:text-white">Admin Panel</h1>
+          <AdminUserList token={token} users={allUsers} />
+        </>
+      ) : (
+        <>
+          <h1 className="text-3xl font-bold mb-6 dark:text-white">Admin Panel</h1>
+          <p className="text-xl dark:text-white">You are not authorized to view this page</p>
+        </>
+      )}
     </div>
   )
 }
