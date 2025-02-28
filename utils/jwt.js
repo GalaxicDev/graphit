87a895a73544c51eca4cbe80471e246488b682a6
nnnac <nils@nnnac.xyz>
2025-02-28 13:34:25 +0100

    adding users

--- utils/jwt.js
+++ utils/jwt.js
@@ -10,6 +10,6 @@ export const verifyToken = (token) => {
     try {
         return jwt.verify(token, process.env.JWT_SECRET);
     } catch (error) {
-        throw new Error('Invalid token');
+        throw new Error(error.message);
     }
 };
